584c99ce2194ea61dd0326474999c2372c798856
mrkrausestem <33553266+mrkrausestem@users.noreply.github.com>
2022-11-11 13:54:11 -0500

    Updated Collections
    Points and improved content

--- lab/Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
+++ lab/Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -1,32 +1,32 @@
 /*:
  ## Exercise - Arrays
  
- Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
+ Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization. (1 point)
  */
 
 
 /*:
- Your friend Sara is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
+ Your friend Sara is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection. (.25 points)
  */
 
 
 /*:
- Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
+ Add four additional names into the array using the compound `+=` operator. All of the names should be added in one step. Print the contents of the collection. (.25 points)
  */
 
 
 /*:
- Use the `insert(_:at:)` method to add `Charlie` into the array as the second element. Print the contents of the collection.
+ Use the `insert(_:at:)` method to add `Charlie` into the array as the second element. Print the contents of the collection. (.25 points)
  */
 
 
 /*:
- Someone had a conflict and decided to transfer her registration to someone else. Use array subscripting to change the sixth element to `Rebecca`. Print the contents of the collection.
+ Someone had a conflict and decided to transfer her registration to someone else. Use array subscripting to change the sixth element to `Rebecca`. Print the contents of the collection. (.25 points)
  */
 
 
 /*:
- Call `removeLast()` on `registrationList`. If done correctly, this should remove `Rebecca` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
+ Call `removeLast()` on `registrationList`. If done correctly, this should remove `Rebecca` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`. (.5 points)
  */
 
 
--- lab/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ lab/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -1,31 +1,31 @@
 /*:
  ## App Exercise - Activity Challenge
  
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ >These exercises reinforce Swift concepts in the context of a math tutoring app.
  
- Your fitness tracking app shows users a list of possible challenges, grouped by activity type (i.e. walking challenges, running challenges, calisthenics challenges, weightlifting challenges, etc.) A challenge could be as simple as "Walk 3 miles a day" or as intense as "Run 5 times a week." 
+ Your app shows users a list of possible challenges, grouped by subject (i.e. geometry challenges, algebra 1 challenges, algebra 2 challenges, calculus challenges, etc.) A challenge could be as simple as "solve 2 step equations" or as intense as "related rates"
  
- Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
+ Using arrays of type `String`, create at least two lists, one for algebra 1 challenges, and one for geometry challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities. (1 point)
  */
 
 
 /*:
- In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
+ In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list. (.5 points)
  */
 
 
 /*:
- All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
+ All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`. (.25 points)
  */
 
 
 /*:
- Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
+ Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it. (.25 points)
  */
 
 
 /*:
- Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
+ Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges." (.5 points)
  */
 
 
